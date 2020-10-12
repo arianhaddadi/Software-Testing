c5fb07517eb7b388205f851390b724d1925ab942
Aryan <a94h.73@gmail.com>
2020-10-12 23:04:35 +0330

    added normal tests for visitsbetween and visituntilage

--- src/test/java/org/springframework/samples/petclinic/owner/PetTestNormal.java
+++ src/test/java/org/springframework/samples/petclinic/owner/PetTestNormal.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import org.springframework.samples.petclinic.visit.Visit;
 
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -21,6 +23,55 @@ public class PetTestNormal {
     public void tearDown() throws Exception {
     	this.pet = null;
     }
+
+	@Test
+	public void getVisitsBetween() {
+		Visit v1 = new Visit().setDate(LocalDate.of(2003, 3,12));
+		this.pet.addVisit(v1);
+		Visit v2 = new Visit().setDate(LocalDate.of(2019, 7,5));
+		this.pet.addVisit(v2);
+		Visit v3 = new Visit().setDate(LocalDate.of(2008, 5,24));
+		this.pet.addVisit(v3);
+		Visit v4 = new Visit().setDate(LocalDate.of(2016, 12,20));
+		this.pet.addVisit(v4);
+		Visit v5 = new Visit().setDate(LocalDate.of(2010, 1,9));
+		this.pet.addVisit(v5);
+		List<Visit> res1 = this.pet.getVisitsBetween(LocalDate.of(2001,2,3), LocalDate.of(2003,1,4));
+		assertEquals(Collections.emptyList(), res1);
+		List<Visit> res2 = this.pet.getVisitsBetween(LocalDate.of(2002,2,3), LocalDate.of(2007,1,4));
+		assertEquals(Collections.singletonList(v1), res2);
+		List<Visit> res3 = this.pet.getVisitsBetween(LocalDate.of(2002,2,3), LocalDate.of(2011,1,4));
+		assertEquals(Arrays.asList(v1, v3, v5), res3);
+		List<Visit> res4 = this.pet.getVisitsBetween(LocalDate.of(2009,2,3), LocalDate.of(2017,1,4));
+		assertEquals(Arrays.asList(v4, v5), res4);
+		List<Visit> res5 = this.pet.getVisitsBetween(LocalDate.of(2001,2,3), LocalDate.of(2010,1,4));
+		assertNotEquals(Arrays.asList(v1, v2, v3, v4, v5), res5);
+	}
+
+	@Test
+	public void getVisitsUntilAge() {
+		this.pet.setBirthDate(LocalDate.of(2000, 10, 9));
+		Visit v1 = new Visit().setDate(LocalDate.of(2003, 3,12));
+		this.pet.addVisit(v1);
+		Visit v2 = new Visit().setDate(LocalDate.of(2019, 7,5));
+		this.pet.addVisit(v2);
+		Visit v3 = new Visit().setDate(LocalDate.of(2008, 5,24));
+		this.pet.addVisit(v3);
+		Visit v4 = new Visit().setDate(LocalDate.of(2016, 12,20));
+		this.pet.addVisit(v4);
+		Visit v5 = new Visit().setDate(LocalDate.of(2010, 1,9));
+		this.pet.addVisit(v5);
+		List<Visit> res1 = this.pet.getVisitsUntilAge(1);
+		assertEquals(Collections.emptyList(), res1);
+		List<Visit> res2 = this.pet.getVisitsUntilAge(4);
+		assertEquals(Collections.singletonList(v1), res2);
+		List<Visit> res3 = this.pet.getVisitsUntilAge(11);
+		assertEquals(Arrays.asList(v1, v3, v5), res3);
+		List<Visit> res4 = this.pet.getVisitsUntilAge(20);
+		assertEquals(Arrays.asList(v1, v2, v3, v4, v5), res4);
+		List<Visit> res5 = this.pet.getVisitsUntilAge(2);
+		assertNotEquals(Arrays.asList(v1, v2, v3, v4, v5), res5);
+	}
 
     @Test
     public void getVisits() {
