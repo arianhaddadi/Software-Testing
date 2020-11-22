5bb0e8a8188b3bb7f1961150885f1d6c137da92f
Aryan <a94h.73@gmail.com>
2020-11-22 23:10:12 +0330

    qweewqw

--- src/test/java/org/springframework/samples/petclinic/utility/TriTypeTest.java
+++ src/test/java/org/springframework/samples/petclinic/utility/TriTypeTest.java
@@ -1,9 +1,6 @@
 package org.springframework.samples.petclinic.utility;
 
-import com.github.mryf323.tractatus.CACC;
-import com.github.mryf323.tractatus.ClauseDefinition;
-import com.github.mryf323.tractatus.UniqueTruePoint;
-import com.github.mryf323.tractatus.Valuation;
+import com.github.mryf323.tractatus.*;
 import com.github.mryf323.tractatus.experimental.extensions.ReportingExtension;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -236,20 +233,362 @@ class TriTypeTest {
 		Assertions.assertEquals(TriType.TryClass.EQUILATERAL, triClass);
 	}
 
+	@ClauseCoverage(
+		predicate = "a + b + c",
+		valuations = {
+			@Valuation(clause = 'a', valuation = false),
+			@Valuation(clause = 'b', valuation = false),
+			@Valuation(clause = 'b', valuation = false)
+		}
+	)
+	@Test
+	public void CC_FirstPredicate_Test1() {
+		TriType tryType = new TriType();
+		TriType.TryClass triClass;
+		triClass = tryType.classifyTriangle(1,1,1);
+		log.debug("triangle identified as {}", triClass);
+		Assertions.assertEquals(TriType.TryClass.EQUILATERAL, triClass);
+	}
+
+	//Here is the second test for the first predicate witch true value for each clause is covered
+	//To have that, all the sides should be negative as we can see in the test. Now for each clause
+	//in the first predicate, true and false values are covered. (CC)
+	@ClauseCoverage(
+		predicate = "a + b + c",
+		valuations = {
+			@Valuation(clause = 'a', valuation = true),
+			@Valuation(clause = 'b', valuation = true),
+			@Valuation(clause = 'b', valuation = true)
+		}
+	)
+	@Test
+	public void CC_FirstPredicate_Test2() {
+		TriType tryType = new TriType();
+		TriType.TryClass triClass;
+		triClass = tryType.classifyTriangle(-1,-2,-3);
+		log.debug("triangle identified as {}", triClass);
+		Assertions.assertEquals(TriType.TryClass.NOT_VALID, triClass);
+	}
+
+	//For the second predicate (Side1+Side2 <= Side3 || Side2+Side3 <= Side1 || Side1+Side3 <= Side2)
+	//like the previous tests for the first predicate, in test 1 we test true values for each clause
+	//and in test 2 we test the false values. 3 tests have been written to make each of them true.
+	@ClauseCoverage(
+		predicate = "d + e + f",
+		valuations = {
+			@Valuation(clause = 'd', valuation = true),
+			@Valuation(clause = 'e', valuation = true),
+			@Valuation(clause = 'f', valuation = true)
+		}
+	)
+	@Test
+	public void CC_SecondPredicate_Test1() {
+		TriType tryType = new TriType();
+		TriType.TryClass triClass;
+		triClass = tryType.classifyTriangle(1,2,4);
+		log.debug("triangle identified as {}", triClass);
+		Assertions.assertEquals(TriType.TryClass.NOT_VALID, triClass);
+		triClass = tryType.classifyTriangle(4,2,1);
+		log.debug("triangle identified as {}", triClass);
+		Assertions.assertEquals(TriType.TryClass.NOT_VALID, triClass);
+		triClass = tryType.classifyTriangle(1,4,2);
+		log.debug("triangle identified as {}", triClass);
+		Assertions.assertEquals(TriType.TryClass.NOT_VALID, triClass);
+	}
+
+	//Here is a second test for clause coverage of second predicate. Here the false values for
+	//clauses have been seen. In the one test below all the clauses will be false and clause coverage
+	//will be completed.
+	@ClauseCoverage(
+		predicate = "d + e + f",
+		valuations = {
+			@Valuation(clause = 'd', valuation = false),
+			@Valuation(clause = 'e', valuation = false),
+			@Valuation(clause = 'f', valuation = false)
+		}
+	)
+	@Test
+	public void CC_SecondPredicate_Test2() {
+		TriType tryType = new TriType();
+		TriType.TryClass triClass;
+		triClass = tryType.classifyTriangle(3,4,5);
+		log.debug("triangle identified as {}", triClass);
+		Assertions.assertEquals(TriType.TryClass.SCALENE, triClass);
+	}
+
+	//The first and second predicate are in form a + b + c so
+	//For a : TFF would be UTP and FFF for NFP
+	//For b : FTF would be UTP and FFF for NFP
+	//For c : FFT would be UTP and FFF for NFP
+	//So for CUTPNFP coverage for these two predicates we should test
+	//{TFF, FTF, FFT, FFF} to satisfy the coverage.
+	//Here is TFF for clause a of first predicate
+	@UniqueTruePoint(
+		predicate = "a + b + c",
+		dnf = "a + b + c",
+		implicant = "a",
+		valuations = {
+			@Valuation(clause = 'a', valuation = true),
+			@Valuation(clause = 'b', valuation = false),
+			@Valuation(clause = 'c', valuation = false)
+		}
+	)
+	@NearFalsePoint(
+		predicate = "a + b + c",
+		dnf = "a + b + c",
+		implicant = "a",
+		clause = 'a',
+		valuations = {
+			@Valuation(clause = 'a', valuation = true),
+			@Valuation(clause = 'b', valuation = false),
+			@Valuation(clause = 'c', valuation = false)
+		}
+	)
+	@Test
+	public void CUTPNFP_FirstPredicate_Test1() {
+		TriType tryType = new TriType();
+		TriType.TryClass triClass;
+		triClass = tryType.classifyTriangle(-1,1,1);
+		log.debug("triangle identified as {}", triClass);
+		Assertions.assertEquals(TriType.TryClass.NOT_VALID, triClass);
+	}
+
+	//Here is FTF for clause b of first predicate
+	@UniqueTruePoint(
+		predicate = "a + b + c",
+		dnf = "a + b + c",
+		implicant = "b",
+		valuations = {
+			@Valuation(clause = 'a', valuation = false),
+			@Valuation(clause = 'b', valuation = true),
+			@Valuation(clause = 'c', valuation = false)
+		}
+	)
+	@NearFalsePoint(
+		predicate = "a + b + c",
+		dnf = "a + b + c",
+		implicant = "b",
+		clause = 'b',
+		valuations = {
+			@Valuation(clause = 'a', valuation = false),
+			@Valuation(clause = 'b', valuation = true),
+			@Valuation(clause = 'c', valuation = false)
+		}
+	)
+	@Test
+	public void CUTPNFP_FirstPredicate_Test2() {
+		TriType tryType = new TriType();
+		TriType.TryClass triClass;
+		triClass = tryType.classifyTriangle(1,-1,1);
+		log.debug("triangle identified as {}", triClass);
+		Assertions.assertEquals(TriType.TryClass.NOT_VALID, triClass);
+	}
+
+	//Here is FFT for clause c of first predicate
+	@UniqueTruePoint(
+		predicate = "a + b + c",
+		dnf = "a + b + c",
+		implicant = "c",
+		valuations = {
+			@Valuation(clause = 'a', valuation = false),
+			@Valuation(clause = 'b', valuation = false),
+			@Valuation(clause = 'c', valuation = true)
+		}
+	)
+	@NearFalsePoint(
+		predicate = "a + b + c",
+		dnf = "a + b + c",
+		implicant = "c",
+		clause = 'c',
+		valuations = {
+			@Valuation(clause = 'a', valuation = false),
+			@Valuation(clause = 'b', valuation = false),
+			@Valuation(clause = 'c', valuation = true)
+		}
+	)
+	@Test
+	public void CUTPNFP_FirstPredicate_Test3() {
+		TriType tryType = new TriType();
+		TriType.TryClass triClass;
+		triClass = tryType.classifyTriangle(1,1,-1);
+		log.debug("triangle identified as {}", triClass);
+		Assertions.assertEquals(TriType.TryClass.NOT_VALID, triClass);
+	}
+
+	//Here is FFF for clause a of first predicate (it is for clause b and c too)
+	@UniqueTruePoint(
+		predicate = "a + b + c",
+		dnf = "a + b + c",
+		implicant = "a",
+		valuations = {
+			@Valuation(clause = 'a', valuation = false),
+			@Valuation(clause = 'b', valuation = false),
+			@Valuation(clause = 'c', valuation = false)
+		}
+	)
+	@NearFalsePoint(
+		predicate = "a + b + c",
+		dnf = "a + b + c",
+		implicant = "a",
+		clause = 'a',
+		valuations = {
+			@Valuation(clause = 'a', valuation = false),
+			@Valuation(clause = 'b', valuation = false),
+			@Valuation(clause = 'c', valuation = false)
+		}
+	)
+	@Test
+	public void CUTPNFP_FirstPredicate_Test4() {
+		TriType tryType = new TriType();
+		TriType.TryClass triClass;
+		triClass = tryType.classifyTriangle(1,1,1);
+		log.debug("triangle identified as {}", triClass);
+		Assertions.assertEquals(TriType.TryClass.EQUILATERAL, triClass);
+	}
+
+	//Here is TFF for clause a of second predicate
+	@UniqueTruePoint(
+		predicate = "d + e + f",
+		dnf = "d + e + f",
+		implicant = "d",
+		valuations = {
+			@Valuation(clause = 'd', valuation = true),
+			@Valuation(clause = 'e', valuation = false),
+			@Valuation(clause = 'f', valuation = false)
+		}
+	)
+	@NearFalsePoint(
+		predicate = "d + e + f",
+		dnf = "d + e + f",
+		implicant = "d",
+		clause = 'd',
+		valuations = {
+			@Valuation(clause = 'd', valuation = true),
+			@Valuation(clause = 'e', valuation = false),
+			@Valuation(clause = 'f', valuation = false)
+		}
+	)
+	@Test
+	public void CUTPNFP_SecondPredicate_Test1() {
+		TriType tryType = new TriType();
+		TriType.TryClass triClass;
+		triClass = tryType.classifyTriangle(1,2,4);
+		log.debug("triangle identified as {}", triClass);
+		Assertions.assertEquals(TriType.TryClass.NOT_VALID, triClass);
+	}
+
+	//Here is FTF for clause b of second predicate
+	@UniqueTruePoint(
+		predicate = "d + e + f",
+		dnf = "d + e + f",
+		implicant = "e",
+		valuations = {
+			@Valuation(clause = 'd', valuation = false),
+			@Valuation(clause = 'e', valuation = true),
+			@Valuation(clause = 'f', valuation = false)
+		}
+	)
+	@NearFalsePoint(
+		predicate = "d + e + f",
+		dnf = "d + e + f",
+		implicant = "e",
+		clause = 'e',
+		valuations = {
+			@Valuation(clause = 'd', valuation = false),
+			@Valuation(clause = 'e', valuation = true),
+			@Valuation(clause = 'f', valuation = false)
+		}
+	)
+	@Test
+	public void CUTPNFP_SecondPredicate_Test2() {
+		TriType tryType = new TriType();
+		TriType.TryClass triClass;
+		triClass = tryType.classifyTriangle(4,1,2);
+		log.debug("triangle identified as {}", triClass);
+		Assertions.assertEquals(TriType.TryClass.NOT_VALID, triClass);
+	}
+
+	//Here is FFT for clause c of second predicate
+	@UniqueTruePoint(
+		predicate = "d + e + f",
+		dnf = "d + e + f",
+		implicant = "f",
+		valuations = {
+			@Valuation(clause = 'd', valuation = false),
+			@Valuation(clause = 'e', valuation = false),
+			@Valuation(clause = 'f', valuation = true)
+		}
+	)
+	@NearFalsePoint(
+		predicate = "d + e + f",
+		dnf = "d + e + f",
+		implicant = "f",
+		clause = 'f',
+		valuations = {
+			@Valuation(clause = 'd', valuation = false),
+			@Valuation(clause = 'e', valuation = false),
+			@Valuation(clause = 'f', valuation = true)
+		}
+	)
+	@Test
+	public void CUTPNFP_SecondPredicate_Test3() {
+		TriType tryType = new TriType();
+		TriType.TryClass triClass;
+		triClass = tryType.classifyTriangle(1,4,2);
+		log.debug("triangle identified as {}", triClass);
+		Assertions.assertEquals(TriType.TryClass.NOT_VALID, triClass);
+	}
+
+	//Here is FFF for clause a of second predicate (it is for clause b and c too)
+	@UniqueTruePoint(
+		predicate = "d + e + f",
+		dnf = "d + e + f",
+		implicant = "d",
+		valuations = {
+			@Valuation(clause = 'd', valuation = false),
+			@Valuation(clause = 'e', valuation = false),
+			@Valuation(clause = 'f', valuation = false)
+		}
+	)
+	@NearFalsePoint(
+		predicate = "d + e + f",
+		dnf = "d + e + f",
+		implicant = "d",
+		clause = 'd',
+		valuations = {
+			@Valuation(clause = 'd', valuation = false),
+			@Valuation(clause = 'e', valuation = false),
+			@Valuation(clause = 'f', valuation = false)
+		}
+	)
+	@Test
+	public void CUTPNFP_SecondPredicate_Test4() {
+		TriType tryType = new TriType();
+		TriType.TryClass triClass;
+		triClass = tryType.classifyTriangle(3,4,5);
+		log.debug("triangle identified as {}", triClass);
+		Assertions.assertEquals(TriType.TryClass.SCALENE, triClass);
+	}
+
 
 	/**
-	 * TODO
-	 * explain your answer here
-	 * @param  a
-	 * @param b
-	 * @param c
-	 * @param d
-	 * @param e
-	 * @return
+	 f = ab + cd (example is in slides given in class)
+	 -> UTP for ab = {TTFF, TTFT, TTTF}, UTP for cd = {FFTT, FTTT, TFTT}
+	 -> clause a: UTP:TTFT -> NFP = FTFT
+	 -> clause b: UTP:TTFT -> NFP = TFFT
+	 -> clause c: UTP:TFTT -> NFP = TFFT
+	 -> clause d: UTP:TFTT -> NFP = TFTF
+	 -> CUTPNFP TRs = {TTFT, FTFT, TFFT, TFTT, TFTF}
+
+	 ~f = ~a~c + ~a~d + ~b~c + ~b~d
+	 CUTPNFP does not contain any UT for ~a~d which is an implicant for ~f and is needed for UTPC
+
+	 -> CUTPNFP does not subsume UTPC
+
 	 */
 	private static boolean questionTwo(boolean a, boolean b, boolean c, boolean d, boolean e) {
 		boolean predicate = false;
-//		predicate = a predicate with any number of clauses
+		predicate = (a & b) || (c & d);
 		return predicate;
 	}
 }
