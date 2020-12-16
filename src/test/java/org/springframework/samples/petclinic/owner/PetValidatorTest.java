b91fe6c42a54ff0f7008cab5aed97b267bb5cff0
Aryan <a94h.73@gmail.com>
2020-12-16 22:56:06 +0330

    Added mutators

--- src/test/java/org/springframework/samples/petclinic/owner/PetValidatorTest.java
+++ src/test/java/org/springframework/samples/petclinic/owner/PetValidatorTest.java
@@ -1,26 +1,81 @@
 package org.springframework.samples.petclinic.owner;
 
-import org.junit.jupiter.api.AfterEach;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.mockito.Mock;
+import org.springframework.validation.Errors;
 
-class PetValidatorTest {
+import javax.validation.constraints.AssertFalse;
 
-    @BeforeEach
-    void setUp() {
-    }
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
 
-    @AfterEach
-    void tearDown() {
-    }
+public class PetValidatorTest {
 
-    @Test
-    void validate() {
-    }
+	private static final String REQUIRED = "required";
 
-    @Test
-    void supports() {
-    }
+	private PetValidator petValidator;
+
+	private Pet petMock;
+
+	private Errors errorsMock;
+
+	@BeforeEach
+	public void setUp() throws Exception {
+		petMock = mock(Pet.class);
+		errorsMock = mock(Errors.class);
+		petValidator = new PetValidator();
+	}
+
+	@Test
+	public void should_errorsReqajectValueNameWhenNameIsEmpty_validate() {
+		doReturn(null).when(petMock).getName();
+		petValidator.validate(petMock, errorsMock);
+		verify(errorsMock, times(1)).rejectValue("name", REQUIRED, REQUIRED);
+	}
+
+
+	@Test
+	public void should_errorsDontRejectValueAnythingWhenNameIsSomething_validate() {
+		doReturn("name").when(petMock).getName();
+		petValidator.validate(petMock, errorsMock);
+		verify(errorsMock, times(0)).rejectValue("name", REQUIRED, REQUIRED);
+	}
+
+	@Test
+	public void should_errorsRejectValueTypeWhenTypeIsNull_validate() {
+		doReturn(null).when(petMock).getType();
+		doReturn(true).when(petMock).isNew();
+		petValidator.validate(petMock, errorsMock);
+		verify(errorsMock, times(1)).rejectValue("type", REQUIRED, REQUIRED);
+	}
+
+	@Test
+	public void should_errorsDontRejectValueAnythingWhenTypeIsNotNull_validate() {
+		doReturn(new PetType()).when(petMock).getType();
+		doReturn(true).when(petMock).isNew();
+		petValidator.validate(petMock, errorsMock);
+		verify(errorsMock, times(0)).rejectValue("type", REQUIRED, REQUIRED);
+	}
+
+	@Test
+	public void should_errorsRejectValueBirthDateWhenBirthIsNull_validate() {
+		doReturn(null).when(petMock).getBirthDate();
+		petValidator.validate(petMock, errorsMock);
+		verify(errorsMock, times(1)).rejectValue("birthDate", REQUIRED, REQUIRED);
+	}
+
+	@Test
+	public void should_returnFalseWhenClassIsNotAssignable_support() {
+		assertFalse(petValidator.supports(Errors.class));
+	}
+
+	@Test
+	public void should_returnTrueWhenClassIsAssignable_support() {
+		assertTrue(petValidator.supports(Pet.class));
+	}
+
 }
