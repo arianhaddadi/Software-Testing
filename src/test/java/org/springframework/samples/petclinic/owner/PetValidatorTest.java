517a8832fb36b70a6be86f4eccd63b3e077db821
Aryan <a94h.73@gmail.com>
2020-12-16 18:57:59 +0330

    added test class and mutators to pom.xml

---
+++ src/test/java/org/springframework/samples/petclinic/owner/PetValidatorTest.java
@@ -0,0 +1,26 @@
+package org.springframework.samples.petclinic.owner;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PetValidatorTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void validate() {
+    }
+
+    @Test
+    public void supports() {
+    }
+}
